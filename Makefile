4539f27e75341ece99336f9645edf059262e1434
Mihunchik <arinushkin048@mail.ru>
2025-02-27 12:05:15 +0300

    adds webpack, makefile, bootstrap, babel

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ install:
 	npm install && npm link
 lint:
 	npx eslint .
+prod:
+	npm run build:prod
+dev:
+	npm run build:dev
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default (env) => ({
+  mode: env.mode ?? 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
+  ],
+});
